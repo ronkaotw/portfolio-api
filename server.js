918788368df4421f4c268636df42b1ccfd6a2ea5
AaronKao <aaronkao88@gmail.com>
2024-12-14 22:54:10 +0800

    最終修復: cors 跨域問題

--- server.js
+++ server.js
@@ -45,7 +45,12 @@ async function connDB() {
 // 中介軟體執行 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-app.use(cors({ origin: "https://api.ronkao.tw" }));
+const corsOptions ={
+    origin:'http://localhost:5173', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(morgan('Aaron'));
  // API 執行
  app.use('/users', apiUser);
